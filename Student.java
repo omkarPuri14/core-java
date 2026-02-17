ad673dd550d9701de66bb62d615c9d0173180bf1
omkarPuri14 <omkar567puri@gmail.com>
2026-02-17 07:29:52 -0800

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,27 @@
+package progam.core;
+
+public class Student {
+
+	public static void main(String[] args) {
+
+		byte Std_age = 19;
+		short Std_id = 23452;
+		int Std_eligbit_no = 24243455;
+		long Std_mobile_no = 6454454352l;
+		float Std_markes = 87.67f;
+		double Std_hight = 6.55;
+		boolean Cheek_pass = true;
+		char Std_char = '@';
+
+		System.out.println("student age =" + Std_age);
+		System.out.println("student id no =" + Std_id);
+		System.out.println("student eligbity no=" + Std_eligbit_no);
+		System.out.println("student Mobile no =" + Std_mobile_no);
+		System.out.println("student Marks =" + Std_markes);
+		System.out.println("student hight =" + Std_hight);
+		System.out.println("student is passing cheek =" + Cheek_pass);
+		System.out.println("student chara  =" + Std_char);
+
+	}
+
+}
