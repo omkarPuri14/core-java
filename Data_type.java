b283bbfc1b6d0b673752ce8c6d29d8ab1f7e0fa4
omkarPuri14 <omkar567puri@gmail.com>
2026-02-19 08:03:38 -0800

    Add files via upload

---
+++ Data_type.java
@@ -0,0 +1,37 @@
+package progam.core;
+
+public class Data_type {
+	public static void main(String[] args) {
+		System.out.println("byte size of premetive data type in warrper class");
+		System.out.println("Byte size is " + Byte.BYTES + " Byte ");
+		System.out.println("Short size is " + Short.BYTES + " Byte ");
+		System.out.println("Integer size is " + Integer.BYTES + " Byte ");
+		System.out.println("Long size is " + Long.BYTES + " Byte ");
+		System.out.println("Float size is " + Float.BYTES + " Byte ");
+		System.out.println("Double size is " + Double.BYTES + " Byte ");
+		System.out.println("Character size is " + Character.BYTES + " Byte ");
+		System.out.println("Boolean is not Found ... !");
+		System.out.println();
+
+		System.out.println("Byte size is " + Byte.SIZE + "bites");
+		System.out.println("short size is" + Short.SIZE + "bites");
+		System.out.println("int size is" + Integer.SIZE + "bites");
+		System.out.println("long size is" + Long.SIZE + "bites");
+		System.out.println("float size is" + Float.SIZE + "bites");
+		System.out.println("dobule size is" + Double.SIZE + "bites");
+		System.out.println("char size is" + Character.SIZE + "bites");
+		System.out.println("Boolean size is not found ");
+		System.out.println();
+
+		System.out.println("byte min value " + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE + " byte max value");
+		System.out.println("short min value " + Short.MIN_VALUE + " to " + Short.MAX_VALUE + " short max value");
+		System.out.println("int min value " + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE + " int max value");
+		System.out.println("long min value " + Long.MIN_VALUE + " to " + Long.MAX_VALUE + " long max value");
+		System.out.println("float min value " + Float.MIN_VALUE + " to " + Float.MAX_VALUE + " float max value");
+		System.out.println("double min value " + Double.MIN_VALUE + " to " + Double.MAX_VALUE + " double max value");
+		System.out.println((int) Character.MIN_VALUE + " to " + (int) Character.MAX_VALUE);
+		System.out.println("Boolean Not Found");
+
+	}
+
+}
