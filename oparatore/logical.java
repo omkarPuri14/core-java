18593d80db593e6b4ac4fb070b6565fc8b51c39a
omkarPuri14 <omkar567puri@gmail.com>
2026-02-27 19:42:01 -0800

    Add files via upload

---
+++ oparatore/logical.java
@@ -0,0 +1,18 @@
+package program.operater;
+
+public class logical {
+		
+	public static void main(String[] args) {
+		 boolean check=true;
+
+		
+		System.out.println(10 > 45 && 20 < 9 || check && true);//true
+		System.out.println((true) || 0 < 9 || check || 0 > 0);//true
+	System.out.println(1 > 5 || 2 < 9 || false && 0 == 0);//true
+ 	System.out.println(check && (20 < 9) || 45 != 0 && true);//true
+
+
+		
+	}
+
+}
