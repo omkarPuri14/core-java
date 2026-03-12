aba2df6fc2556b22bf2aa98776359a29cf991253
omkarPuri14 <omkar567puri@gmail.com>
2026-03-12 12:33:18 -0700

    Add files via upload

---
+++ Control_Flow/Loops.java
@@ -0,0 +1,429 @@
+package program.control_flow;
+
+public class Loops {
+	public static void main(String[] args) {
+//	1)Print numbers from 1 to 10 using a loop.
+//		int i=1;
+//				for(i=1;i<=10;i++) {
+//					System.out.println(i);
+//					
+//				}
+//				while(i<=10) {
+//					System.out.println(i);
+//					i++;
+//				}
+//				do {
+//					System.out.println(i);
+//					i++;
+//				}while(i<=10);
+			
+// 2) Print number from 10 to 1	
+//		
+//		int i=10;
+//				for(i=10;i>=1;i--) {
+//					System.out.println(i);
+//					
+//				}
+//				while(i>=1) {
+//					System.out.println(i);
+//					i--;
+//				}
+//				do {
+//					System.out.println(i);
+//					i--;
+//				}while(i>=1);
+		
+//		3)Print all even numbers between 1 and 50.
+//		4)Print all odd numbers between 1 and 50.
+//		int i=1;
+//		for(i=1;i<=50;i++) {
+//			switch(i%2) {
+//			case 0:
+//				System.out.println(i+" is even no ");
+//				break;
+//			case 1:
+//				System.out.println(i+ " is odd no ");
+//				break;
+//				
+//			}
+//			if(i%2==0) {
+//				System.out.println(i+" is even no");
+//				
+//			}else {
+//				System.out.println(i+" is odd no");
+//			}
+//			
+//			
+//			
+//		}
+//		while(i<=50) {
+//			switch(i%2) {
+//			case 0:
+//				System.out.println(i+" is even no ");
+//				break;
+//			case 1:
+//				System.out.println(i+ " is odd no ");
+//				break;
+//				}
+//			i++;
+//			if(i%2==0) {
+//				System.out.println(i+" is even no");
+//				
+//			}else {
+//				System.out.println(i+" is odd no");
+//			}
+//			i++;
+//			
+//			
+//		}
+//		do {
+//			switch(i%2) {
+//			case 0:
+//				System.out.println(i+" is even no ");
+//				break;
+//			case 1:
+//				System.out.println(i+ " is odd no ");
+//				break;
+//				}
+//			i++;
+//			if(i%2==0) {
+//				System.out.println(i+" is even no");
+//				
+//			}else {
+//				System.out.println(i+" is odd no");
+//			}
+//			i++;
+//			
+//		}while(i<=50);
+		
+		
+//		5) Print the table of 5 using a loop.
+//		int i=1;
+//		for(i=1;i<=10;i++) {
+//			System.out.println(5+"*"+i+"="+5*i);
+//			
+//		}
+//		while(i<=10) {
+//			System.out.println(5+"*"+i+"="+5*i);
+//			i++;
+//			
+//		}
+//		do {
+//			System.out.println(5+"*"+i+"="+5*i);
+//			i++;
+//		}while(i<=10);
+		
+		
+//		6) Print the table of any number (e.g., n = 7).
+//		int i=1;
+//		int table=5;
+//		for(i=1;i<=10;i++) {
+//			switch(table) {
+//			case 1:
+//				System.out.println(1+"*"+i+"="+1*i);
+//				break;
+//			case 2:
+//				System.out.println(2+"*"+i+"="+2*i);
+//				break;
+//			case 3:
+//				System.out.println(3+"*"+i+"="+3*i);
+//				break;
+//			case 4:
+//				System.out.println(4+"*"+i+"="+4*i);
+//				break;
+//			case 5:
+//				System.out.println(5+"*"+i+"="+5*i);
+//				break;
+//			case 6:
+//				System.out.println(6+"*"+i+"="+6*i);
+//				break;
+//			case 7:
+//				System.out.println(7+"*"+i+"="+7*i);
+//				break;
+//			case 8:
+//				System.out.println(8+"*"+i+"="+8*i);
+//				break;
+//			case 9:
+//				System.out.println(9+"*"+i+"="+9*i);
+//				break;
+//			case 10:
+//				System.out.println(10+"*"+i+"="+10*i);
+//				break;
+//				
+//			}
+//						
+//		}
+//		while(i<=10) {
+//			switch(table) {
+//			case 1:
+//				System.out.println(1+"*"+i+"="+1*i);
+//				i++;
+//				break;
+//			case 2:
+//				System.out.println(2+"*"+i+"="+2*i);
+//				i++;
+//				break;
+//			case 3:
+//				System.out.println(3+"*"+i+"="+3*i);
+//				i++;
+//				break;
+//			case 4:
+//				System.out.println(4+"*"+i+"="+4*i);
+//				i++;
+//				break;
+//			case 5:
+//				System.out.println(5+"*"+i+"="+5*i);
+//				i++;
+//				break;
+//			case 6:
+//				System.out.println(6+"*"+i+"="+6*i);
+//				i++;
+//				break;
+//			case 7:
+//				System.out.println(7+"*"+i+"="+7*i);
+//				i++;
+//				break;
+//			case 8:
+//				System.out.println(8+"*"+i+"="+8*i);
+//				i++;
+//				break;
+//			case 9:
+//				System.out.println(9+"*"+i+"="+9*i);
+//				i++;
+//				break;
+//			case 10:
+//				System.out.println(10+"*"+i+"="+10*i);
+//				i++;
+//				break;
+//		default:
+//			System.out.println("invalid input");
+//				
+//			}
+//			
+//			
+//		}
+//		do {
+//			switch(table) {
+//			case 1:
+//				System.out.println(1+"*"+i+"="+1*i);
+//				i++;
+//				break;
+//			case 2:
+//				System.out.println(2+"*"+i+"="+2*i);
+//				i++;
+//				break;
+//			case 3:
+//				System.out.println(3+"*"+i+"="+3*i);
+//				i++;
+//				break;
+//			case 4:
+//				System.out.println(4+"*"+i+"="+4*i);
+//				i++;
+//				break;
+//			case 5:
+//				System.out.println(5+"*"+i+"="+5*i);
+//				i++;
+//				break;
+//			case 6:
+//				System.out.println(6+"*"+i+"="+6*i);
+//				i++;
+//				break;
+//			case 7:
+//				System.out.println(7+"*"+i+"="+7*i);
+//				i++;
+//				break;
+//			case 8:
+//				System.out.println(8+"*"+i+"="+8*i);
+//				i++;
+//				break;
+//			case 9:
+//				System.out.println(9+"*"+i+"="+9*i);
+//				i++;
+//				break;
+//			case 10:
+//				System.out.println(10+"*"+i+"="+10*i);
+//				i++;
+//				break;
+//				
+//			}
+//			
+//		}while(i<=10);
+
+//		7) Print Hello 10 times.
+//		int i=1;
+//		for(i=1;i<=10;i++) {
+//			System.out.println(i+" hello");
+//		}
+//		while(i<=10) {
+//			System.out.println(i+" hello");
+//			i++;
+//		}
+//		do {
+//			System.out.println(i+" hello");
+//			i++;
+//			
+//		}while(i<=10);
+		
+//		8) Print the sum of first 10 natural numbers.
+//		int i=1;
+//		int num=10;
+//		int sum=0;
+//		for(i=1;i<=num;i++) {
+//			sum+=i;
+//		System.out.println("The sum of the first " + num + " natural numbers is: " + sum);
+//		}
+//		while(i<=num) {
+//			sum+=i;
+//			System.out.println("The sum of the first " + num + " natural numbers is: " + sum);
+//			
+//			i++;
+//			
+//		}
+//		do {
+//			sum+=i;
+//			System.out.println("The sum of the first " + num + " natural numbers is: " + sum);
+//			
+//			i++;
+//			
+//		}while(i<=num);
+//		9) Print the sum of even numbers from 1 to 100.
+//		int i=1;
+//		int sum=0;
+//		int num=100;
+//		for(i=1;i<=num;i++) {
+//			if(i%2==0) {
+//				sum+=i;
+//				System.out.println("the sum of the first "+num+" even number is: "+sum);
+//			}else {
+//				System.out.println();
+//			}
+//			switch(i%2) {
+//			case 0:
+//				sum+=i;
+//				System.out.println("the sum of the first "+num+" even number is: "+sum);
+//				break;
+//				default:
+//					System.out.println();
+//			}
+			
+//		}
+//		while(i<=100) {
+//
+//			if(i%2==0) {
+//				sum+=i;
+//				System.out.println("the sum of the first "+num+" even number is: "+sum);
+//				
+//		
+//			}else {
+//				System.out.println();
+//			}
+//			switch(i%2) {
+//			case 0:
+//				sum+=i;
+//				System.out.println("the sum of the first "+num+" even number is: "+sum);
+//				break;
+//				default:
+//					System.out.println();
+//			}
+//			
+//			i++;
+//			
+//			
+//			
+//		}
+//		do {
+//			if(i%2==0) {
+//				sum+=i;
+//				System.out.println("the sum of the first "+num+" even number is: "+sum);
+//				
+//		
+//			}else {
+//				System.out.println();
+//			}
+//			switch(i%2) {
+//			case 0:
+//				sum+=i;
+//				System.out.println("the sum of the first "+num+" even number is: "+sum);
+//				break;
+//				default:
+//					System.out.println();
+//			}
+//			
+//			i++;
+//			
+//		}while(i<=num);		
+//		10) Print the sum of odd numbers from 1 to 100.
+		int i=1;
+		int sum=0;
+		int num=100;
+//		for(i=1;i<=num;i++) {
+//			if(i%2!=0) {
+//				sum+=i;
+//				System.out.println("the sum of the first "+num+" odd number is: "+sum);
+//			}else {
+//				System.out.println();
+//			}
+//			switch(i%2) {
+//			case 1:
+//				sum+=i;
+//				System.out.println("the sum of the first "+num+" odd number is: "+sum);
+//				break;
+//				default:
+//					System.out.println();
+//			}
+//			
+//		}
+//		while(i<=100) {
+//			
+//						if(i%2!=0) {
+//							sum+=i;
+//							System.out.println("the sum of the first "+num+" even number is: "+sum);
+//							
+//					
+//						}else {
+//							System.out.println();
+//						}
+//						switch(i%2) {
+//						case 1:
+//							sum+=i;
+//							System.out.println("the sum of the first "+num+" even number is: "+sum);
+//							break;
+//							default:
+//								System.out.println();
+//						}
+//						
+//						i++;
+//						
+//						
+//						
+//					}
+		do {
+			while(i<=100) {
+				
+				if(i%2!=0) {
+					sum+=i;
+					System.out.println("the sum of the first "+num+" even number is: "+sum);
+					
+			
+				}else {
+					System.out.println();
+				}
+				switch(i%2) {
+				case 1:
+					sum+=i;
+					System.out.println("the sum of the first "+num+" even number is: "+sum);
+					break;
+					default:
+						System.out.println();
+				}
+				
+				i++;
+				
+				
+				
+			}
+		}while(i<=num);
+
+	}
+
+}
