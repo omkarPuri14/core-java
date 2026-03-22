71af2b20b8552f78fa5eb183653a37ab781eaa4a
omkarPuri14 <omkar567puri@gmail.com>
2026-03-22 11:19:26 -0700

    Add files via upload

---
+++ Control_Flow/Loops_Q.java
@@ -0,0 +1,78 @@
+package program.control_flow;
+
+public class Loops_Q {
+	public static void main(String[] args) {
+
+//		 reverse no input =1234 out put=4321
+//		 int num=10;
+//		 int rnum=0;
+//		 
+//		 while(num!=0) {
+//			 int reminder=num%10;
+//			 
+//			 rnum=rnum*10+reminder;
+//			 num/=10;
+//			 
+//			 
+//			 
+//		 }
+//		 System.out.println(rnum);
+
+		// palindrome number with fix value
+//		int revno = 0;
+//		int num = 1;
+//		int temp = num;
+//		while (temp > 0) {
+//			int reminder = temp % 10;
+//			revno = revno * 10 + reminder;
+//			temp /= 10;
+//		}
+//		
+//		if(revno==num) {
+//			System.out.println(num+" palinder number");
+//
+//			
+//		}else {
+//			System.out.println(num+" not palinder number ");
+//		}
+
+		// prime number
+//		int num=29;
+//		int count=0;
+//		if(num<=1) {
+//			System.out.println(" not a prime no");
+//			
+//		}
+//		
+//		for(int i=2;i*i<=num;i++) {
+//			if(num%i==0) {
+//				count++;
+//				break;
+//			}
+//			
+//			
+//		}
+//		if(count==0) {
+//			System.out.println(num+ " is primre number");
+//			
+//		}else {
+//			System.out.println(num+" is not prime number");
+//		}
+
+//		for (int i = 1; i <= 100; i++) {
+//			int count = 0;
+//
+//			for (int j = 1; j <= i; j++) {
+//				if (i % j == 0) {
+//					count++;
+//				}
+//			}
+//
+//			if (count == 2) {
+//				System.out.println(i + "  primre no");
+//			}
+//		}
+
+			}
+
+}
