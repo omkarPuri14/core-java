8abd8d284bb55dc6ebc38e1b6be3cca82da7bc2d
omkarPuri14 <omkar567puri@gmail.com>
2026-04-09 20:38:33 -0700

    Add files via upload

---
+++ Scanner/Pratic_Q.java
@@ -0,0 +1,235 @@
+package com.scaner;
+
+import java.util.Scanner;
+
+public class Pratic_Q {
+	public static void main(String[] args) {
+		// addition of two no
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("enter first no");
+//		
+//		int num1=sc.nextInt();
+//		
+//		System.out.println("enter sce no");
+//		int num2=sc.nextInt();
+//		
+//		int sum=num1+num2;
+//		System.out.println("sum of "+num1+" +"+num2+" ="+sum);
+//		
+//		sc.close();
+
+		// area of circle
+
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("enter the radius");
+//		double r=sc.nextDouble();
+//		
+//		double pi=3.143;
+//		double area=pi*r*r;
+//		System.out.println("area of circle is "+ area);
+
+//		Scanner sc =new Scanner(System.in);
+//		System.out.println("enter your age ");
+//		int age= sc.nextInt();
+//		System.out.println("user age is "+age);
+//		
+//		sc.close();
+
+		// larger of three
+
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("enter first no");
+//		int num1=sc.nextInt();
+//		System.out.println("enter scecond no");
+//		int num2=sc.nextInt();
+//		System.out.println("enter third no");
+//		int num3=sc.nextInt();
+//		
+//		if(num1>num2&&num1>num3) {
+//			System.out.println(num1+" is larger no");
+//			
+//		}else if(num2>num1&&num2>num3) {
+//			System.out.println(num2+" is larger no");
+//		}else {
+//			System.out.println(num3+" is larger no");
+//		}
+//		sc.close();
+
+		// print user age
+//		Scanner sc =new Scanner(System.in);
+//		System.out.println("enter your age ");
+//		int age= sc.nextInt();
+//		System.out.println("user age is "+age);
+//		
+//		sc.close();
+
+		// Take a sentence from user using nextLine() and print it.
+
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("enter any sentance");
+//		String s=sc.nextLine();
+//		System.out.println(s);
+
+		// Take marks of 5 subjects and print total & percentage.
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("enter the marks");
+//
+//		System.out.println("math");
+//        double mark1 = sc.nextDouble();
+//		
+//
+//		System.out.println("science");
+//
+//		double mark2 = sc.nextDouble();
+//		
+//
+//		System.out.println("english");
+//
+//		double mark3 = sc.nextDouble();
+//		
+//
+//		System.out.println("java");
+//
+//		double mark4 = sc.nextDouble();
+//		
+//
+//		System.out.println("marathi");
+//
+//		double mark5 = sc.nextDouble();
+//		
+//		
+//		double total=mark1+mark2+mark3+mark4+mark5;
+//		System.out.println("total maeks in 5 subjet "+total);
+//		
+//		double percent=(total/500)*100;
+//		System.out.println("percent = "+percent);
+		
+		//Take length and width and calculate area of rectangle.
+		
+//		Scanner sc=new Scanner(System.in);
+//		
+//	   System.out.println("enter length");
+//		
+//		double length=sc.nextDouble();
+//		System.out.println("enter width");
+//		double width=sc.nextDouble();
+//		
+//		double area=length*width;
+//		System.out.println("area of rectangle "+area);
+//		
+//		sc.close();
+		
+		//take a number and check if it is even or odd.
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("enter a number ");
+//		
+//		int num=sc.nextInt();
+//		
+//		if(num%2==0) {
+//			System.out.println(num+" is even no");
+//			
+//		}else {
+//			System.out.println(num+" is odd no");
+//		}
+		
+		// Take a number and check if it is positive, negative, or zero.
+		
+//		Scanner sc=new Scanner(System.in);
+//		
+//		System.out.println("enter the  no");
+//		
+//		int num=sc.nextInt();
+//		
+//		if(num>0) {
+//			System.out.println(num+" is positive");
+//		}else if(num<0) {
+//			System.out.println(num+" is negative");
+//		}else {
+//			System.out.println(num+" is zero");
+//		}
+		
+		//Take two numbers and print multiplication & division.
+		
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("enter first no");
+//		
+//		int num1=sc.nextInt();
+//		
+//		System.out.println("enter sce no");
+//		int num2=sc.nextInt();
+//		
+//		int multiplation=num1*num2;
+//		System.out.println("multiplation of "+num1+" *"+num2+" ="+multiplation);
+//		int division=num1/num2;
+//		System.out.println("devision of "+num1+" /"+num2+" ="+division);
+//		
+//		sc.close();
+		
+		//Take temperature in Celsius and convert to Fahrenheit
+		
+//		Scanner sc=new Scanner(System.in);
+//		
+//		System.out.println("enter tempeture");
+//		double temp=sc.nextInt();
+//		double fahrenheit=(temp - 32)*5/9;
+//		
+//		System.out.println("tempeature to fahrenheit "+fahrenheit+"f");
+		
+		// Take the price and quantity of a product and print total bill.
+		
+		
+//		Scanner sc=new Scanner(System.in);
+//		
+//		System.out.println("enter the price");
+//		
+//		double price=sc.nextDouble();
+//		System.out.println("enter quantity");
+//		int quenty=sc.nextInt();
+//		
+//		double bill=price*quenty;
+//		System.out.println("total bill is "+bill);
+		
+		//Take employee name, id, and salary and print employee details.
+		
+//		Scanner sc=new Scanner(System.in);
+//		
+//		System.out.println("enter emp name ");
+//		String emp_Name=sc.nextLine();
+//		System.out.println("enter emp id");
+//		int id=sc.nextInt();
+//		System.out.println("enter emp salary");
+//		double sal=sc.nextDouble();
+//		
+//		System.out.println("emp name "+emp_Name);
+//		System.out.println("emp id "+id);
+//		System.out.println("enter emp salary "+sal);
+		
+		//Take two strings using nextLine() and print their concatenation.
+		
+//		Scanner sc=new Scanner(System.in);
+//		
+//		System.out.println("enter first String");
+//		String first=sc.nextLine();
+//		System.out.println("enter scecond string");
+//		String scecond=sc.nextLine();
+//		System.out.println(first+scecond);
+		
+		
+		
+		
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
