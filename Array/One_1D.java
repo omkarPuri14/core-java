58265c4be51060ad61574fc0c7c06008b7e78862
omkarPuri14 <omkar567puri@gmail.com>
2026-04-28 13:38:13 +0530

    Add files via upload

---
+++ Array/One_1D.java
@@ -0,0 +1,189 @@
+package com.array;
+
+import java.util.Arrays;
+
+public class One_1D {
+	public static void main(String[] args) {
+		// 1) Write a program to declare and initialize a 1D array of integers.
+
+//		int []a= {1,2,3,4,5,};
+//		for(int i=0;i<a.length;i++) {
+//			
+//			System.out.println(a[i]);
+//			
+//		}
+
+		// 2) Write a program to print all elements of a 1D array.
+//		String [] names= {"omkar","karan","saloni","kajal","guru"};
+//		
+//		for(int i=0;i<names.length;i++) {
+//			
+//			System.out.println(names[i]);
+//			
+//		}
+
+		// 3) Write a program to find the length of a 1D array.
+
+//		int []b= {1,2,3,4,5,6};
+//		System.out.println(b.length);
+
+		// 4) Write a program to find the sum of all elements in a 1D array.
+
+//		int []numbers= {1,2,3,4,5,6};
+//		int sum = Arrays.stream(numbers).sum();
+//		System.out.println(sum);
+
+		// 5) Write a program to find the average of elements in a 1D array.
+//		int []numbers= {1,2,3,4,5};
+//		int sum = Arrays.stream(numbers).sum();
+//		int avg=sum/numbers.length;
+//		System.out.println("avg of this array is "+avg);
+
+		// 6) Write a program to find the maximum element in a 1D array.
+//		int[] num = { 10, 20, 45, 86, 34, 95 };
+//
+//		Arrays.sort(num);
+//		int max = num[num.length - 1];
+//
+//		System.out.println("Maximum element of array: " + max);
+
+		// 7) Write a program to find the minimum element in a 1D array.
+//		int[] num = { 10, 20, 45, 86, 34, 95 };
+//
+//		Arrays.sort(num);
+//		int min = num[0];
+//		System.out.println("minimum element of array "+min);
+
+		// 8) Write a program to search an element in a 1D array.
+
+//		int[] num = { 10, 20, 45, 86, 34, 95 };
+//		int search=34;
+//        int a= Arrays.binarySearch(num, search);
+//        if (a >= 0) {
+//            System.out.println("Element found at index: " + a);
+//        } else {
+//            System.out.println("Element not found.");
+//        }
+
+		// 9) Write a program to count even and odd numbers in a 1D array.
+//		int[] num = { 10, 20, 45, 86, 34, 95 };
+//		
+//				for(int i=0;i<num.length;i++) {
+//					
+//					if(num[i]%2==0) {
+//						System.out.println("number is even "+num[i]);
+//						
+//					}else {
+//						System.out.println("number is odd "+num[i]);
+//					}
+//					
+//				}
+
+		// 10) Write a program to copy elements from one array to another.
+
+//		int[] num = { 10, 20, 45, 86, 34, 95 };
+//		int[]num2= Arrays.copyOf(num, num.length);
+//		
+//		System.out.println(Arrays.toString(num2));
+
+		// 11) Write a program to reverse a 1D array.
+
+//		int[] num = { 10, 20, 45, 86, 34, 95 };
+//
+//		for (int i = 5; i < num.length; i--) {
+//			if (i > -1) {
+//				System.out.println(num[i]);
+//
+//			}
+//
+//		}
+		// 12) Write a program to sort a 1D array in ascending order.
+
+//      		int[] num = { 10, 20, 45, 86, 34, 95 };
+//      	    Arrays.sort(num);
+//      	    System.out.println(Arrays.toString(num));
+
+		// 13) Write a program to sort a 1D array in descending order.
+//		int[] num = { 10, 20, 45, 86, 34, 95 };
+//
+//		Arrays.sort(num);
+//
+//		for (int i = 5; i < num.length; i--) {
+//
+//			if (i > -1) {
+//				System.out.println(num[i]);
+//
+//			}
+//
+//		}
+		
+		// 14 Write a program to find duplicate elements in a 1D array.
+		
+//		int[] num = { 10, 20, 20,45,45, 86, 34, 95 };
+//		
+//		 for(int i = 0; i < num.length; i++) {
+//	            for(int j = i + 1; j < num.length; j++) {
+//	                if(num[i] == num[j]) {
+//	                    System.out.println(num[j]);
+//	                    
+//	                }
+//	            }
+//	        }
+		
+		//15 Write a program to remove duplicate elements from a 1D array.
+		
+		
+		// 16
+		
+		//17 Write a program to find the second largest element in a 1D array
+		
+//		int[] num = { 10, 20, 45, 86, 34, 95 };
+//		Arrays.sort(num);
+//	
+//		int scelar=num[num.length-2];
+//		System.out.println(scelar);
+		
+		// 18 Write a program to find the second smallest element in a 1D array.
+//		int[] num = { 10, 20, 45, 86, 34, 95 };
+//		Arrays.sort(num);
+//		
+//		int secsmall=num[1];
+//		System.out.println(secsmall);
+		
+		//19 Write a program to merge two 1D arrays.
+		
+//		int [] num= {23,45,56,6};
+//		int [] num2= {6,4,5,7};
+		
+		// 20 Write a program to check whether a 1D array is a palindrome.
+		
+		
+		
+		
+		
+		
+		
+
+
+		
+		
+
+		
+		
+		
+		
+		
+		
+	                
+	            
+	            
+	            
+	        
+		
+		
+		
+		
+
+
+	}
+}
