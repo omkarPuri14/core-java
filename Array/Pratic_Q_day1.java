65469abc5a45caaa320619df639bed5f541e0732
omkarPuri14 <omkar567puri@gmail.com>
2026-05-04 17:06:37 +0530

    Add files via upload

---
+++ Array/Pratic_Q_day1.java
@@ -0,0 +1,70 @@
+package com.array;
+
+public class Pratic_Q_day1 {
+	public static void main(String[] args) {
+		
+		//sum of 2d array
+
+//		int[][] num = { { 12, 34, 45 }, { 46, 34, 45 }, { 23, 34, 45 } };
+//		int sum = 0;
+//
+//		for (int i = 0; i < num.length; i++) {
+//
+//			for (int j = 0; j < num.length; j++) {
+//				sum = sum + num[i][j];
+//
+//			}
+//
+//		}
+//		System.out.println(sum);
+
+		// 2d EVEN ODD
+
+//		int[][] num = { { 12, 34, 45 }, { 46, 34, 45 }, { 23, 34, 45 } };
+//
+//		for (int i = 0; i < num.length; i++) {
+//
+//			for (int j = 0; j < num.length; j++) {
+//				if (num[i][j] % 2 == 0) {
+//					System.out.println("number is even " + num[i][j]);
+//				} else {
+//					System.out.println("number is odd " + num[i][j]);
+//
+//				}
+//
+//			}
+//
+//		}
+		// 1d array questions 
+		
+		// sum of 1D array
+//		int[] num = { 10, 20, 20, 45, 45, 86, 34, 95 };
+//		int sum=0;
+//		
+//		
+//		for(int i=0;i<num.length;i++) {
+//			
+//			int target=num[i];
+//			sum=sum+target;
+//			
+//			
+//		}
+//		System.out.println(sum);
+
+		// even odd 1D array
+
+//		int[] num = { 10, 20, 45, 86, 34, 95 };
+//	
+//				for(int i=0;i<num.length;i++) {
+//					
+//					if(num[i]%2==0) {
+//						System.out.println("number is even "+num[i]);
+//						
+//					}else {
+//						System.out.println("number is odd "+num[i]);
+//					}
+//					
+//				}
+	}
+
+}
