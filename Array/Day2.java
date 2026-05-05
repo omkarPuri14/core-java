8a22f4f9357591b9ac34d10014d41aaf841e3a37
omkarPuri14 <omkar567puri@gmail.com>
2026-05-05 10:50:41 +0530

    Add files via upload

---
+++ Array/Day2.java
@@ -0,0 +1,93 @@
+package com.array;
+
+
+public class Day2 {
+	
+
+
+	public static void main(String[] args) {
+		// 2D array check the element is palendrome or not
+
+//		int [][]num={{121,345,46},{67,212,78},{67,90,65}};
+//		
+//		
+//		for (int i = 0; i < num.length; i++) {
+//            for (int j = 0; j < num.length; j++) {
+//                
+//                int target = num[i][j];
+//                int temp = target;
+//                int sum = 0;
+//
+//              
+//                while (temp > 0) {
+//                    int reminder = temp % 10;
+//                    sum = sum * 10 + reminder;
+//                    temp = temp / 10;
+//                }
+//
+//               
+//                if (target == sum) { 
+//                    System.out.println(target+ " is a palindrome");
+//                }else {
+//                	 System.out.println(target+ " is not a palindrome");
+//                	
+//                }
+//            }
+//        }
+
+		// Check the number is perfect or not in 2d array
+
+//		int[][] num = { { 6, 345, 46 }, { 67, 212, 28 }, { 67, 496, 65 } };
+//
+//		for (int i = 0; i < num.length; i++) {
+//			for (int j = 0; j < num.length; j++) {
+//				int sum = 0;
+//
+//				int target = num[i][j];
+//
+//				if (target > 0) {
+//					for (int k = 1; k < target; k++) {
+//
+//						if (target % k == 0) {
+//							sum = sum + k;
+//
+//						}
+//
+//					}
+//					if (sum == target) {
+//						System.out.println(target + " is a perfect no");
+//					} else {
+//						System.out.println(target + " is not a perfect no");
+//
+//					}
+//
+//				}
+//
+//			}
+//		}
+		
+		// Check the string is empty or not if not then check it length and check the length id odd or even 
+		
+//		String name="ankush";
+//		
+//		if(name.length()>0) {
+//			System.out.println("string is not empty");
+//			if(name.length()%2==0) {
+//				System.out.println(name.length()+" is even length");
+//				
+//			}else {
+//				System.out.println(name.length()+" is odd length");
+//				
+//			}
+//		}
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
